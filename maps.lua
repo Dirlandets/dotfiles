56752bbc0253d5b9b551d907fd7d570aa7d2043d
Alexey K <uuuuiiiaaa@yandex.ru>
2022-02-05 00:28:15 +0300

    Move mappings to sepparate folder

---
+++ maps.lua
@@ -0,0 +1,37 @@
+local opts = { noremap=true, silent=false}
+-- Shorten nvim_set_keymap name
+local keymap = vim.api.nvim_set_keymap
+
+-- Remap leader key to space
+-- keymap("", "<Space>", "<Nop>", opts)
+-- vim.g.mapleader = " "
+-- vim.g.maplocalleader = " "
+
+-- line or visually selected block - alt+j/k
+-- TODO: Alt does not work
+-- inoremap <A-j> <Esc>:m .+1<CR>==gi
+-- inoremap <A-k> <Esc>:m .-2<CR>==gi
+-- vnoremap <A-j> :m '>+1<CR>gv=gv
+-- vnoremap <A-k> :m '<-2<CR>gv=gv
+-- move split panes to left/bottom/top/right
+keymap("n", "<A-h>", "<C-W>h", opts)
+keymap("n", "<A-j>", "<C-W>j", opts)
+keymap("n", "<A-k>", "<C-W>k", opts)
+keymap("n", "<A-l>", "<C-W>l", opts)
+-- move between panes to left/bottom/top/right
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
+-- Navigate buffers
+keymap("n", "<S-l>", ":bnext<CR>", opts)
+keymap("n", "<S-h>", ":bprevious<CR>", opts)
+-- Press i to enter insert mode, and jk(kj) to exit insert mode.
+keymap("i", "jk", "<ESC>", opts)
+keymap("i", "kj", "<ESC>", opts)
+keymap("v", "jk", "<ESC>", opts)
+keymap("v", "kj", "<ESC>", opts)
+-- Press i to enter insert mode, and ii to exit insert mode.
+-- :vnoremap jk <Esc>
+-- :vnoremap kj <Esc>
+
