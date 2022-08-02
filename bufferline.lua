964c4126bd37679b91543cbc277e6a4fb176bc6a
Alexey K <uuuuiiiaaa@yandex.ru>
2022-08-02 11:49:51 +0300

    Add more stuff to bufferline

--- bufferline.lua
+++ bufferline.lua
@@ -25,13 +25,19 @@ require('bufferline').setup {
         return vim.fn.fnamemodify(buf.name, ':t:r')
       end
     end,
-    max_name_length = 18,
+    max_name_length = 30,
     max_prefix_length = 15, -- prefix used when a buffer is de-duplicated
-    tab_size = 18,
-    diagnostics = true,
-    diagnostics_update_in_insert = false,
+    tab_size = 25,
+    diagnostics = 'nvim_lsp',
+    diagnostics_update_in_insert = true,
     diagnostics_indicator = function(count, level, diagnostics_dict, context)
-      return "("..count..")"
+      local s = " "
+      for e, n in pairs(diagnostics_dict) do
+        local sym = e == "error" and " "
+          or (e == "warning" and " " or "i " )
+        s = s .. n .. sym
+      end
+      return s
     end,
     -- NOTE: this will be called a lot so don't do any heavy processing here
     custom_filter = function(buf_number, buf_numbers)
@@ -72,7 +78,7 @@ require('bufferline').setup {
     -- [focused and unfocused]. eg: { '|', '|' }
     separator_style = "thin",
     enforce_regular_tabs = true,
-    always_show_bufferline = false,
+    always_show_bufferline = true,
   }
 }
 
