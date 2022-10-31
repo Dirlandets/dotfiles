5cd500f55455543099d2971cc2b8664f2c0ffd8d
Alexey K <uuuuiiiaaa@yandex.ru>
2022-10-31 18:44:22 +0300

    Increase mypy and flake timeouts

--- lsp/null_ls.lua
+++ lsp/null_ls.lua
@@ -1,8 +1,12 @@
 local null_ls = require('null-ls')
 
 local sources = {
-  null_ls.builtins.diagnostics.mypy,
-  null_ls.builtins.diagnostics.flake8,
+  null_ls.builtins.diagnostics.mypy.with({
+    timeout = 60000,
+  }),
+  null_ls.builtins.diagnostics.flake8.with({
+    timeout = 60000,
+  }),
   null_ls.builtins.diagnostics.eslint_d,
   null_ls.builtins.diagnostics.hadolint,
   -- Codeactions
@@ -11,8 +15,20 @@ local sources = {
   null_ls.builtins.formatting.isort,
   null_ls.builtins.formatting.rustfmt,
 }
-null_ls.setup({
-  diagnostics_format = "#{s}: [#{c}] #{m}",
+null_ls.setup {
+  cmd = { "nvim" },
+  debounce = 250,
   debug = true,
+  default_timeout = 10000,
+  diagnostic_config = nil,
+  diagnostics_format = "#{s}: [#{c}] #{m}",
+  fallback_severity = vim.diagnostic.severity.ERROR,
+  log_level = "warn",
+  notify_format = "[null-ls] %s",
+  on_attach = nil,
+  on_init = nil,
+  on_exit = nil,
+  root_dir = require("null-ls.utils").root_pattern(".null-ls-root", "Makefile", ".git"),
+  update_in_insert = true,
   sources = sources,
-})
+}
